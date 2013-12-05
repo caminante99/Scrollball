e53f971c62fb79424cc81450179474156c8e8b8c
Matthew Goodall <dogsaw@thecyberplains.com>
2013-12-05 17:20:31 +1300

    Send the mousewheel events asynchronously
    The hook really only needs to work out if it should cancel or pass
    through the input.
    
    I did some profiling and it looked like the pinvoke operations are my most
    expensive operations that I can safely move to a new thread.
    
    This should make it less likely to hit the low level hook timeout (see
    remarks in http://msdn.microsoft.com/en-us/library/windows/desktop/ms644985(v=vs.85).aspx)

--- ScrollBall/TrackballScroll.fs
+++ ScrollBall/TrackballScroll.fs
@@ -12,10 +12,6 @@ type TrackballScroll () =
     let mutable y = 0
     let mutable ignoreInput = false
     let hook = new LowLevelMouseHook(fun nCode wParam lParam ->        
-        let dx = x - lParam.pt.x
-        let dy = y - lParam.pt.y
-        let scrollX = (log (float (abs dx))) // * (float ( dx / dx ))
-        let scrollY = (log (float (abs dy)))  //* (float ( dy / dy ))
 #if DEBUG
         printfn "dx %A, dy %A | sx %A, sy %A" dx dy scrollX scrollY
 #endif
@@ -39,23 +35,30 @@ type TrackballScroll () =
             false
         else if scrolling && wParam = WM.MOUSEMOVE then
             scrolled <- true
+            let sendScroll = async {
 
-            if scrollX <> -infinity && scrollX > 0.50 then
-                let steps = (uint32 (scrollX * 60.00)) * if dx < 0 then 1u else 0u - 1u
-#if DEBUG                
-                printfn "scrolling x by %A steps" steps
-#endif
-                SendMouse MOUSEEVENT.HWHEEL 0u 0u  steps
-                |> ignore
+                    let dx = x - lParam.pt.x
+                    let dy = y - lParam.pt.y
+                    let scrollX = (log (float (abs dx)))
+                    let scrollY = (log (float (abs dy)))
+                    if scrollX <> -infinity && scrollX > 0.50 then
+                        let steps = (uint32 (scrollX * 60.00)) * if dx < 0 then 1u else 0u - 1u
+                        #if DEBUG                
+                            printfn "scrolling x by %A steps" steps
+                        #endif
+                        SendMouse MOUSEEVENT.HWHEEL 0u 0u  steps
+                        |> ignore
 
-            if scrollY <> -infinity && scrollY > 0.50 then
-                let steps = (uint32 (scrollY * 60.00)) * if dy > 0 then 1u else 0u - 1u
-#if DEBUG                
-                printfn "scrolling y by %A steps" steps
-#endif
-                SendMouse MOUSEEVENT.WHEEL 0u 0u  steps
-                |> ignore
+                    if scrollY <> -infinity && scrollY > 0.50 then
+                        let steps = (uint32 (scrollY * 60.00)) * if dy > 0 then 1u else 0u - 1u
+                        #if DEBUG                
+                            printfn "scrolling y by %A steps" steps
+                        #endif
+                        SendMouse MOUSEEVENT.WHEEL 0u 0u  steps
+                        |> ignore
+                }
 
+            Async.Start sendScroll
             false
         else
             x <- lParam.pt.x
